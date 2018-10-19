a360077899726a217d0a8d5f9d7941c0e5d1c98c
Jen Stehlik <jen.stehlik@kallysoft.ch>
2018-10-19 15:40:41 +0200

    better mobile support

--- js/timetable.js
+++ js/timetable.js
@@ -4,9 +4,10 @@ $(function () {
     const classUrl = 'https://sandbox.gibm.ch/klassen.php';
     const scheduleUrl = 'http://sandbox.gibm.ch/tafel.php';
 
+    //creates the calendar with all the parameters and data from the api
     function initializeCalendar() {
         $('#calendar').fullCalendar({
-            defaultView: 'agendaWeek',
+            defaultView: 'agendaDay',
             handleWindowResize: true,
             height: 'auto',
             weekends: false,
@@ -16,7 +17,7 @@ $(function () {
             slotLabelFormat: 'HH:mm',
             titleFormat: 'D MMMM YYYY',
             timeFormat: 'HH:mm',
-            header:{
+            header: {
                 left: '',
                 center: 'title',
                 right: 'today prev,next'
@@ -46,21 +47,41 @@ $(function () {
                 });
             },
             eventRender: function (event, element) {
-                element.popover({
-                    title: event.title,
-                    html: true,
-                    content: 'Professor: ' + event.prof + '<br/>Room: ' + event.room + '<br/>Comment: ' + event.comment,
-                    placement: 'right',
-                    delay: {
-                        "show": 0,
-                        "hide": 150
-                    },
-                    trigger: 'hover focus'
+                //if there's a comment add it to the description
+                var description = 'Prof.: ' + event.prof + '<br/>Room: ' + event.room;
+                if (event.comment) {
+                    description += '<br/>Comment: ' + event.comment;
+                }
+                
+                
+                //popovers only for wide enough screens
+                if ($(window).width() > 800) {
+                    element.popover({
+                        title: event.title,
+                        html: true,
+                        content: description,
+                        placement: 'right',
+                        delay: {
+                            "show": 0,
+                            "hide": 150
+                        },
+                        trigger: 'hover focus'
+                    });
+                }
+
+                //mobile users can click on events and it'll show a modal instead of the popover
+                element.swipe({
+                    tap: function (tapevent, target) {
+                        $('#eventDetails .modal-title').html(event.title);
+                        $('#eventDetails .modal-body').html(description);
+                        $('#eventDetails').modal('show');
+                    }
                 });
             }
         });
     }
 
+    //populates the job select dropdown from the api
     function populateJobDrop() {
         $('#slctJob').html('<option selected>Select job</option>');
         $.getJSON(jobUrl, function (data) {
@@ -71,12 +92,13 @@ $(function () {
         }).done(function () {
             if (Cookies.get('job')) {
                 $("#slctJob option:selected").removeAttr('selected');
-                $("#slctJob option[value="+Cookies.get('job')+"]").prop('selected', true);
+                $("#slctJob option[value=" + Cookies.get('job') + "]").prop('selected', true);
                 populateClassDrop(Cookies.get('job'));
             }
         });
     }
 
+    //populates the class select dropdown from the api depending on the job id
     function populateClassDrop(jobId) {
         $('#slctClass').html('<option selected>Select class</option>');
         $.getJSON(classUrl, { beruf_id: jobId }, function (data) {
@@ -87,17 +109,26 @@ $(function () {
         }).done(function () {
             if (Cookies.get('job')) {
                 $("#slctClass option:selected").removeAttr('selected');
-                $("#slctClass option[value="+Cookies.get('class')+"]").prop('selected', true);
+                $("#slctClass option[value=" + Cookies.get('class') + "]").prop('selected', true);
             }
         });
     }
 
+    //sets the cookies for the current choice and updates the calendar
     function saveSettings() {
-        Cookies.set('job', $("#slctJob option:selected").val(), { expires: 7 });
-        Cookies.set('class', $("#slctClass option:selected").val(), { expires: 7 });
+        Cookies.set('job', $("#slctJob option:selected").val(), { expires: 8 });
+        Cookies.set('class', $("#slctClass option:selected").val(), { expires: 8 });
         $('#settings').modal('hide');
-        //initializeCalendar();
         $('#calendar').fullCalendar('refetchEvents');
+    }
+
+    //changes the calendar to only show a day on screens smaller than 800px 
+    function switchDayWeek() {
+        if ($(window).width() < 800) {
+            $('#calendar').fullCalendar('changeView', 'agendaDay');
+        } else {
+            $('#calendar').fullCalendar('changeView', 'agendaWeek');
+        }
     }
 
     //Event Handler
@@ -106,11 +137,28 @@ $(function () {
     $('#slctJob').change(function () {
         populateClassDrop($("#slctJob option:selected").val());
     });
+    $(window).resize(switchDayWeek);
+    $('#calendar').swipe({
+        swipe: function (event, direction, distance, duration, fingerCount) {
+            if (direction == 'left') {
+                $('.fc-view-container').animate({ opacity: 0 }, 200, 'linear', function () {
+                    $('#calendar').fullCalendar('next');
+                    $('.fc-view-container').animate({ opacity: 100 }, 200, 'linear');
+                });
+            } else if (direction == 'right') {
+                $('.fc-view-container').animate({ opacity: 0 }, 200, 'linear', function () {
+                    $('#calendar').fullCalendar('prev');
+                    $('.fc-view-container').animate({ opacity: 100 }, 200, 'linear');
+                });
+            }
 
-    //Check for cookies on load
-    if (Cookies.get('class')) {
-        initializeCalendar();
-    } else {
+        }
+    });
+
+    //Onload stuff
+    initializeCalendar();
+    switchDayWeek();
+    if (!Cookies.get('class')) {
         $('#settings').modal('show');
         populateJobDrop();
     }
